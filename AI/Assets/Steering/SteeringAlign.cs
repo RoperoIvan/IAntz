e9ba5f3d00af28dcab8387b8f68aad915698d476
Gollim <virtualhowl@gmail.com>
2019-10-30 19:41:43 +0100

    AI Second steering movment
    Steering Align (some work to do)
    Velocity Matching

--- AI/Assets/Steering/SteeringAlign.cs
+++ AI/Assets/Steering/SteeringAlign.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class SteeringAlign : MonoBehaviour {
 
 	public float min_angle = 0.01f;
-	public float slow_angle = 0.1f;
+	public float slow_angle = 10.0f;
 	public float time_to_target = 0.1f;
+    public float slow_factor;
 
-	Move move;
+
+    Move move;
 
 	// Use this for initialization
 	void Start () {
@@ -21,5 +23,41 @@ public class SteeringAlign : MonoBehaviour {
         // Find the desired rotation and accelerate to it
         // Use Vector3.SignedAngle() to find the angle between two directions
 
+        float current_rotation;
+        float desired_rotation;
+        float correct_rotation;
+        float slow_factor;
+
+        Vector3 vec_t_m;
+        Vector3 vec_my_front;
+        Vector3 vec_to_accel;
+
+        current_rotation = move.rotation;
+
+        vec_t_m = move.target.transform.position - this.transform.position;
+        vec_my_front = this.transform.forward;
+
+        desired_rotation = Vector3.SignedAngle(vec_my_front, vec_t_m, Vector3.up);
+
+        if (desired_rotation <= min_angle && desired_rotation >= -min_angle)
+            move.SetRotationVelocity(0);
+        else
+        {
+            //if (desired_rotation <= slow_angle && desired_rotation >= -slow_angle)
+            //    slow_factor = desired_rotation / slow_angle;
+            //else
+            //    slow_factor = 1;
+            //desired_rotation *= slow_factor;
+
+            correct_rotation = (desired_rotation - current_rotation) / time_to_target;
+
+            correct_rotation = Mathf.Clamp(correct_rotation, -move.max_rot_acceleration, move.max_rot_acceleration);
+
+            move.AccelerateRotation(correct_rotation);
+            move.AccelerateMovement(this.transform.forward);
+
+        }
+        
+       
     }
 }
--- AI/Assets/Steering/SteeringVelocityMatching.cs
+++ AI/Assets/Steering/SteeringVelocityMatching.cs
@@ -21,7 +21,21 @@ public class SteeringVelocityMatching : MonoBehaviour {
 		{
             // TODO 8: First come up with your ideal velocity
             // then accelerate to it.
+            Vector3 vec_desired = target_move.movement;
+            Vector3 vec_my_vel = move.movement;
+            Vector3 final_acc;
 
+            Vector3 accelerate_to_match = (vec_desired - vec_my_vel) / time_to_accel;
+            final_acc = accelerate_to_match.normalized;
+
+            float Clamped = Mathf.Clamp(accelerate_to_match.magnitude, -move.max_mov_acceleration, move.max_mov_acceleration);
+
+            final_acc *= Clamped;
+
+            move.AccelerateMovement(final_acc);
+
+
+            
         }
 	}
 }
