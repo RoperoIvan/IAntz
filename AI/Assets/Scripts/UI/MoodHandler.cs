7970c4a0465cdfe3b903d877d286f1369285f4f1
RoperoIvan <ivan_ropero98@hotmail.com>
2019-11-29 17:41:40 +0100

    UI Almost completed

---
+++ AI/Assets/Scripts/UI/MoodHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoodHandler : MonoBehaviour
+{
+    public Sprite[] mood;
+    public Image mood_img;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mood_img.sprite = mood[GetComponent<MoodManager>().current_mood];
+    }
+}
---
+++ AI/Assets/Scripts/UI/ResourceHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceHandler : MonoBehaviour
+{
+    public Sprite[] resource;
+    public Image resource_img;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        resource_img.sprite = resource[GetComponent<ResourceManager>().current_resource];
+    }
+}
