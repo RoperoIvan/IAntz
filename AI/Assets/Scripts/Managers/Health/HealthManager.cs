46f861b63b42d5456af3f2541b35cb7594633691
Gollim <virtualhowl@gmail.com>
2019-11-30 18:25:24 +0100

    Mood Manager

--- AI/Assets/Scripts/Managers/Health/HealthManager.cs
+++ AI/Assets/Scripts/Managers/Health/HealthManager.cs
@@ -6,15 +6,21 @@ public class HealthManager : MonoBehaviour
 {
     public int health;
     public int current_health;
+    MoodManager Mmanager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Mmanager = this.GetComponent<MoodManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (current_health == health)
+            Mmanager.current_mood = 0;
+
+        if (current_health == health / 2)
+            Mmanager.current_mood = 1;
+
     }
 }
