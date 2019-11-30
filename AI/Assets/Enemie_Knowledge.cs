1e0a13167c0e8cee5d172762982f5924d6b25e77
Gollim <virtualhowl@gmail.com>
2019-11-30 23:28:37 +0100

    Enemie almost finished

--- AI/Assets/Enemie_Knowledge.cs
+++ AI/Assets/Enemie_Knowledge.cs
@@ -8,20 +8,28 @@ public class Enemie_Knowledge : MonoBehaviour
     //GameObjects:
     public GameObject Queen;
     public GameObject My_current_enemy;
+    public GameObject enemi_anthill;
+    public GameObject My_spawn;
+    
+    public bool day;
     
 
     AIVision my_vision;
+    private Day_night life_cycle;
 
     // Start is called before the first frame update
-    
+
     void Start()
     {
+        life_cycle = enemi_anthill.GetComponent<Day_night>();
         my_vision = this.GetComponent<AIVision>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        day = life_cycle.day;
+
         My_current_enemy = null;
         My_current_enemy = my_vision.current_enemy;
     }
