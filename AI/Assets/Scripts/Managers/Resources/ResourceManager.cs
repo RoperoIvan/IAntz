5b4db4ad1a562fd3c4f9b421d1abb8a2fda02252
Gollim <virtualhowl@gmail.com>
2019-11-30 18:15:27 +0100

    Worker_finished

--- AI/Assets/Scripts/Managers/Resources/ResourceManager.cs
+++ AI/Assets/Scripts/Managers/Resources/ResourceManager.cs
@@ -6,15 +6,16 @@ public class ResourceManager : MonoBehaviour
 {
     public int resource_type;
     public int current_resource;
+    Worker_Knowledge my_knowledg;
     // Start is called before the first frame update
     void Start()
     {
-        
+        my_knowledg = this.GetComponent<Worker_Knowledge>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        current_resource = my_knowledg.my_load;
     }
 }
