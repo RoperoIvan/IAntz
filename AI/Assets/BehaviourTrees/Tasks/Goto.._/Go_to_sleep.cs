95886ad2eb30cbd3f7a6b7e77b4d131b838a502c
Gollim <virtualhowl@gmail.com>
2019-12-01 18:10:24 +0100

    Now when workers go to sleep chose a random location in the dorm

--- AI/Assets/BehaviourTrees/Tasks/Goto.._/Go_to_sleep.cs
+++ AI/Assets/BehaviourTrees/Tasks/Goto.._/Go_to_sleep.cs
@@ -15,7 +15,7 @@ namespace AI{
 
         SteeringAlign align;
         Move move;
-
+        Vector3 helper;
         protected override string OnInit(){
             align = my_ant.value.GetComponent<SteeringAlign>();
             move = my_ant.value.GetComponent<Move>();
@@ -23,11 +23,44 @@ namespace AI{
         }
 
 		protected override void OnExecute(){
-			
-		}
+            Collider to_work;
+            Bounds to_w;
+            Vector3 mA;
+            Vector3 mI;
+
+            to_work = my_sleep_module.value.GetComponent<Collider>();
+
+            to_w = to_work.bounds;
+
+            mI = to_w.min;
+            mA = to_w.max;
+
+            float largo = mA.x - mI.x;
+            float ancho = mA.z - mI.z;
+
+            Debug.Log("Largo ahora =" + largo);
+
+            float r_h = Random.value;
+            float r_w = Random.value;
+
+            largo *= r_h;
+            ancho *= r_h;
+            Debug.Log("Largo ahora =" + largo);
+
+            Vector3 rand_final;
+            rand_final.x = largo;
+            rand_final.z = ancho;
+            rand_final.y = mI.y;
+
+            mI.x += largo;
+            mI.z += ancho;
+            mI.y = my_ant.value.transform.position.y;
+
+            helper = mI;
+        }
 
 		protected override void OnUpdate(){
-            align.DrivetoTarget(my_sleep_module.value.transform.position, 3);
+            align.DrivetoTarget(helper, 3);
             if (on_sleep_module.value)
             {
                 EndAction(true);
