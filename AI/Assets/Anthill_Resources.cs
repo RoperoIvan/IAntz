e2af1b4cfe3c4a4384c2431b8e95dde643c58c7c
Gollim <virtualhowl@gmail.com>
2019-12-12 20:27:06 +0100

    Now resources will be seen also in base

--- AI/Assets/Anthill_Resources.cs
+++ AI/Assets/Anthill_Resources.cs
@@ -27,6 +27,34 @@ public class Anthill_Resources : MonoBehaviour
     public GameObject queen;
 
     public int Total_cantity;
+
+
+
+    //Base Resource Managment.
+    public GameObject Rock_b_1;
+    public GameObject Rock_b_2;
+    public GameObject Rock_b_3;
+
+    public GameObject Food_b_1;
+    public GameObject Food_b_2;
+    public GameObject Food_b_3;
+
+    public GameObject Wood_b_1;
+    public GameObject Wood_b_2;
+    public GameObject Wood_b_3;
+
+    public int Rock_c_1;
+    public int Rock_c_2;
+    public int Rock_c_3;
+
+    public int Food_c_1;
+    public int Food_c_2;
+    public int Food_c_3;
+
+    public int Wood_c_1;
+    public int Wood_c_2;
+    public int Wood_c_3;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -52,6 +80,22 @@ public class Anthill_Resources : MonoBehaviour
 
         //Percent_branches = Total_cantity - Branches_cantity;
 
+        if (Input.GetKeyDown("p"))
+        {
+            Rocks_cantity += 100;
+        }
+
+        if (Input.GetKeyDown("o"))
+        {
+            Food_cantity += 100;
+        }
+
+        if (Input.GetKeyDown("i"))
+        {
+            Branches_cantity += 100;
+        }
+
+
         // UI.
         resources_food.text = "Food:" + Food_cantity;
         resources_rocks.text = "Rocks:" + Rocks_cantity;
@@ -61,6 +105,8 @@ public class Anthill_Resources : MonoBehaviour
         {
             you_lose_canvas.SetActive(true);
         }
+
+        CheckResources();
     }
 
     //public void ChangePriority(int resource, int priority)
@@ -93,5 +139,70 @@ public class Anthill_Resources : MonoBehaviour
         Percent_food = (Priority_food * 100) / Totalpriority;
         Percent_rocks = (Priority_rocks * 100) / Totalpriority;
         Percent_branches = (Priority_branches * 100) / Totalpriority;
+    }
+
+    private void CheckResources()
+    {
+        //First we set to false all the gameobjects.
+
+        Rock_b_1.SetActive(false);
+        Rock_b_2.SetActive(false);
+        Rock_b_3.SetActive(false);
+
+        Food_b_1.SetActive(false);
+        Food_b_2.SetActive(false);
+        Food_b_3.SetActive(false);
+
+        Wood_b_1.SetActive(false);
+        Wood_b_2.SetActive(false);
+        Wood_b_3.SetActive(false);
+
+        //Then we check if we are over the first quantity.
+        if (Rocks_cantity >= Rock_c_1)
+        {
+            Rock_b_1.SetActive(true);
+            //Theb we check if we are over the second check.
+            if (Rocks_cantity >= Rock_c_2)
+            {
+                Rock_b_2.SetActive(true);
+                //Last we check the if we are over the third.
+                if (Rocks_cantity >= Rock_c_3)
+                {
+                    Rock_b_3.SetActive(true);
+                }
+            }
+        }
+
+        //Then we check if we are over the first quantity.
+        if (Food_cantity >= Food_c_1)
+        {
+            Food_b_1.SetActive(true);
+            //Theb we check if we are over the second check.
+            if (Food_cantity >= Food_c_2)
+            {
+                Food_b_2.SetActive(true);
+                //Last we check the if we are over the third.
+                if (Food_cantity >= Food_c_3)
+                {
+                    Food_b_3.SetActive(true);
+                }
+            }
+        }
+
+        //Then we check if we are over the first quantity.
+        if (Branches_cantity >= Wood_c_1)
+        {
+            Wood_b_1.SetActive(true);
+            //Theb we check if we are over the second check.
+            if (Branches_cantity >= Wood_c_2)
+            {
+                Wood_b_2.SetActive(true);
+                //Last we check the if we are over the third.
+                if (Branches_cantity >= Wood_c_3)
+                {
+                    Wood_b_3.SetActive(true);
+                }
+            }
+        }
     }
 }
