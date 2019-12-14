ced3ba7bab6bb544631e6e3b726cc1ef076353bb
Gollim <virtualhowl@gmail.com>
2019-12-14 18:53:40 +0100

    Some more errors fixed (warrior and worker)

--- AI/Assets/BehaviourTrees/Tasks/Goto.._/Go_to_sleep.cs
+++ AI/Assets/BehaviourTrees/Tasks/Goto.._/Go_to_sleep.cs
@@ -23,37 +23,9 @@ namespace AI{
         }
 
 		protected override void OnExecute(){
-            Collider to_work;
-            Bounds to_w;
-            Vector3 mA;
-            Vector3 mI;
 
-            to_work = my_sleep_module.value.GetComponent<Collider>();
 
-            to_w = to_work.bounds;
-
-            mI = to_w.min;
-            mA = to_w.max;
-
-            float largo = mA.x - mI.x;
-            float ancho = mA.z - mI.z;
-
-            float r_h = Random.value;
-            float r_w = Random.value;
-
-            largo *= r_h;
-            ancho *= r_h;
-
-            Vector3 rand_final;
-            rand_final.x = largo;
-            rand_final.z = ancho;
-            rand_final.y = mI.y;
-
-            mI.x += largo;
-            mI.z += ancho;
-            mI.y = my_ant.value.transform.position.y;
-
-            helper = mI;
+            helper = my_sleep_module.value.transform.position;
         }
 
 		protected override void OnUpdate(){
